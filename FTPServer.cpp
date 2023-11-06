a01fa7c9989e15a8cf8c093c55acec139af9471d
Peyman Shabani <peyman.shabani@ut.ac.ir>
2023-11-06 12:20:52 +0330

    still working with filesystem namespace with some new functionalities

--- FTPServer.cpp
+++ FTPServer.cpp
@@ -7,10 +7,28 @@ using namespace std;
 
 class FileSystem{
     public:
+        FileSystem(string dirPath){
+            workingDirectory = dirPath;
+            this->changeWorkingDirectory(dirPath);
+        }
         void RenameFile(const char* oldName, const char* newName){
             rename( oldName, newName);
         }
-
+        void PrintWorkingDirectory(){
+            // cout << "Current path is " << filesystem::current_path() << endl; 
+            cout << "Current path is: " << this->workingDirectory << endl;
+        }
+        void listFiles(){
+            //std::string path = "/path/to/directory";
+            for (const auto & entry : filesystem::directory_iterator(this->workingDirectory))
+                std::cout << entry.path() << entry.path().filename() <<std::endl;
+        }
+        void changeWorkingDirectory(const filesystem::path newPath){
+            filesystem::current_path(newPath);
+        }
+    private:
+        string workingDirectory;
+        //string rootDirectory;
     
 };
 
@@ -36,7 +54,11 @@ class FTPServer{
 int main() {
     //FTPServer server;
     //server.getCommand();
-    FileSystem filesys;
-    filesys.RenameFile("test1.txt", "test2.txt");
+    FileSystem filesys("/home/peyman/Desktop/CPP/FTP_Server/FTPSERVER/serverDrive");
+    //filesys.RenameFile("test1.txt", "test2.txt");
+    //filesys.WorkingDirectory();
+    //filesys.listFiles();
+    filesys.changeWorkingDirectory("dir2");
+    filesys.PrintWorkingDirectory();
     return 0;
 }
